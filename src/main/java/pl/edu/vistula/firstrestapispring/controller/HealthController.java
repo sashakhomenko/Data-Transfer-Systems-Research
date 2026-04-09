73e62f4fa71bdf5db8cebf7833967d1e44c00711
sashakhomenko <alex.olegovych@gmail.com>
2026-04-09 12:04:04 +0200

    Added: health controller

---
+++ src/main/java/pl/edu/vistula/firstrestapispring/controller/HealthController.java
@@ -0,0 +1,12 @@
+package pl.edu.vistula.firstrestapispring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
